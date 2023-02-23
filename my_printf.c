555de371b7fa519b5508da78f4aafe3bffa5adf7
danny ortiz <ortiz_d@qwasar.io>
2023-02-23 21:54:27 +0000

    updated to use write() syscall

--- my_printf.c
+++ my_printf.c
@@ -3,12 +3,21 @@
 #include <unistd.h>
 #include <stdarg.h>
 
-int my_printf(char * restrict format, ...)
+// void my_printf(char* format, ...);
+char* convert(unsigned int, int);
+
+// int main()
+// {
+//     my_printf("%s", "Hello World\n");
+//     return 0;
+// }
+
+int my_printf(char* format, ...)
 {
     unsigned int i;
     char* input;
     char* s;
-    char* convert(unsigned int, int);
+    void* p;
 
     va_list args;
     va_start(args, format);
@@ -17,60 +26,73 @@ int my_printf(char * restrict format, ...)
     {
         while(*input != '%')
         {
-            putchar(*input);
-            input++;
+                putchar(*input);
+                input++;
         }
-        
         input++;
         
-        {
             //FETCH & EXECUTE VARIABLE CONVERSION
-            switch(*input)
-            {
-                case 'd' :
-                {
-                    i = va_arg(args, int);
-                    if(i<0)
-                    {
-                        i = -1;
-                        putchar('-');
-                    }
-                    puts(convert(i,10));
-                    break;
-                }
+        switch(*input)
+        {
+                case 'd' : i = va_arg(args, int);
+                                if(i<=0)
+                                {
+                                    i = -i;
+                                    write(1, "-", 1);
+                                }
+                                write(1, convert(i, 10), strlen(convert(i, 10)));
+                                break;
+                
+                case 'o' : i = va_arg(args, unsigned int);
+                                write(1, convert(i, 8),  strlen(convert(i, 8)));
+                                break;
+             
 
-                case 'o' :
-                {
-                    format[i] = va_arg(args, unsigned octal);
-                }
+                case 'u' : i = va_arg(args, unsigned int);
+                                write(1, convert(i, 10), strlen(convert(i, 10)));
+                                break;
+                
 
-                case 'u' :
-                {
-                    format[i] = va_arg(args, unsigned decimal);
-                }
+                case 'x' : i = va_arg(args, unsigned int);
+                                write(1, convert(i, 16), strlen(convert(i, 16)));
+                                break;
+                
 
-                case 'x' :
-                {
-                    format[i] = va_arg(args, unsigned hex);
-                }
+                case 'c' : i = va_arg(args, int);
+                                write(1, &i, 1);
+                                break;
 
-                case 'c' :
-                {
-                    i = va_arg(args, int);
-                    putchar(i);
-                    break;
-                }
 
-                case 's' :
-                {
-                    format[i] = va_arg(args, char*);
-                }
+                case 's' : s = va_arg(args, char*);
+                                write(1, &s, strlen(s));
+                                break;
+                
 
-                case 'p' :
-                {
-                    format[i] = va_arg(args, void*);
-                }
-            }
+                case 'p' : p = va_arg(args, void*);
+                                intptr_t ptr_val = (intptr_t)p;
+                                write(1, convert(ptr_val, 16), strlen(convert(ptr_val, 16)));
+                                break;
         }
     }
+    va_end(args);
+    return 0;
+}
+
+char* convert(unsigned int n, int base)
+{
+            static char Rep[] = "0123456789ABCDEF";
+            static char buffer[50];
+            char *ptr;
+
+            ptr = &buffer[49];
+            *ptr = '\0';
+
+            do
+            {
+                *--ptr = Rep[n%base];
+                n /= base;
+            }
+            while(n!=0);
+        
+            return(ptr);
 }
